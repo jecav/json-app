42b66a820f34f07ee28eaa76ab73336003ab6d7b
Jorge Cavazos <jorge.cav@live.com>
2018-02-25 21:59:25 -0600

    Update 02
    Added instructions and Save option.

--- src/index.js
+++ src/index.js
@@ -8,18 +8,29 @@ class MainApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputText: '{"test": "true"}',
+      inputText: '',
       inputJSON: {}
     }
     this.loadInput = this.loadInput.bind(this);
     this.saveInput = this.saveInput.bind(this);
   }
-  // componentWillUpdate(){
-  //   console.log('App will update...');
-  // }
-  // componentDidUpdate(){
-  //   console.log('App updated...');
-  // }
+
+  componentWillMount() {
+    //Load saved data...
+    let savedData = localStorage.getItem("savedJSON");
+    let defaultData = '[{"test1": "true"},{"test2": "true"},{"test3": "true"}]';
+
+    if(savedData){
+      this.setState ({
+        inputText: savedData
+      });
+    } else {
+      this.setState ({
+        inputText: defaultData
+      });
+    }
+  }
+
   componentDidMount() {
     console.log('App mounted...');
 
@@ -32,10 +43,17 @@ class MainApp extends React.Component {
 
   loadInput(){
     let textInput = document.getElementById("inputArea").value;
+    let parsedInput;
+
+    try {
+      parsedInput = JSON.parse(textInput);
+    } catch (e) {
+      parsedInput = {"error": "Bad JSON"};
+    }
 
     this.setState ({
       inputText: textInput,
-      inputJSON: JSON.parse(textInput)
+      inputJSON: parsedInput
     });
 
     console.log(this.state.inputText);
@@ -43,7 +61,7 @@ class MainApp extends React.Component {
   }
 
   saveInput(){
-
+    localStorage.setItem("savedJSON", this.state.inputText);
   }
 
   render() {
@@ -51,9 +69,14 @@ class MainApp extends React.Component {
     return (
       <div>
         <div>
-          <textarea id='inputArea' rows="4" cols="50">{this.state.inputText}</textarea>
+          <textarea id='inputArea' defaultValue={this.state.inputText}></textarea>
           <div id='outputArea'></div>
         </div>
+        <p>Instructions:
+          <br />Edit JSON on the left side, when ready press Load to parse it.
+          <br />You are able to save this JSON for future reference by pressing Save.
+        </p>
+        <hr />
         <button id='LoadBtn'>Load</button>
         <button id='SaveBtn'>Save</button>
       </div>
