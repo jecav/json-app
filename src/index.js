ca1e5fbb127b89b0320ebf4e42129ca1ebddbfbf
Jorge Cavazos <jorge.cav@live.com>
2018-02-26 23:01:39 -0600

    Update 03

--- src/index.js
+++ src/index.js
@@ -13,12 +13,28 @@ class MainApp extends React.Component {
     }
     this.loadInput = this.loadInput.bind(this);
     this.saveInput = this.saveInput.bind(this);
+    this.parseJSON = this.parseJSON.bind(this);
   }
 
   componentWillMount() {
     //Load saved data...
     let savedData = localStorage.getItem("savedJSON");
-    let defaultData = '[{"test1": "true"},{"test2": "true"},{"test3": "true"}]';
+    let defaultData =
+`[
+  {
+    "test1.1": "AAA",
+    "test1.2": "AAA",
+    "test1.3": "AAA"
+  },
+  {
+    "test2.1": "BBB"
+  },
+  {
+    "test3.1": "CCC",
+    "test3.2": "CCC",
+    "test3.3": "CCC"
+  }
+]`;
 
     if(savedData){
       this.setState ({
@@ -32,7 +48,7 @@ class MainApp extends React.Component {
   }
 
   componentDidMount() {
-    console.log('App mounted...');
+    // console.log('App mounted...');
 
     //Event listeners...
     const loadBtn = document.getElementById('LoadBtn');
@@ -56,29 +72,53 @@ class MainApp extends React.Component {
       inputJSON: parsedInput
     });
 
-    console.log(this.state.inputText);
-    console.log(this.state.inputJSON);
+    // console.log(this.state.inputText);
+    // console.log(this.state.inputJSON);
   }
 
   saveInput(){
     localStorage.setItem("savedJSON", this.state.inputText);
   }
 
+  parseJSON(obj){
+    let output = "";
+
+    for(let key in obj){
+
+      if(obj[key] !== null && typeof obj[key] === 'object') {
+        // console.log("Obj-" + key);
+        output += "Obj-" + key + "\n";
+        output += this.parseJSON(obj[key]);
+      } else {
+        // console.log("Key: " + key);
+        // console.log("Value: " + obj[key]);
+        output += "Key: " + key + "\n";
+        output += "Value: " + obj[key] + "\n";
+      }
+    }
+
+    // console.log(output);
+    return output;
+  }
+
   render() {
+    let outputText = this.parseJSON(this.state.inputJSON);
+    // console.log(outputText);
 
     return (
       <div>
-        <div>
-          <textarea id='inputArea' defaultValue={this.state.inputText}></textarea>
-          <div id='outputArea'></div>
+        <div className='container'>
+          <textarea id='inputArea' defaultValue={this.state.inputText} />
+          <textarea id='outputArea'value={outputText} />
         </div>
-        <p>Instructions:
-          <br />Edit JSON on the left side, when ready press Load to parse it.
-          <br />You are able to save this JSON for future reference by pressing Save.
+        <p className='instructions'>
+          Instructions:
+          <br />Edit JSON on the left side, when ready press ENTER to parse it.
+          <br />You are able to save this JSON for future reference by pressing SAVE.
         </p>
         <hr />
-        <button id='LoadBtn'>Load</button>
-        <button id='SaveBtn'>Save</button>
+        <button id='LoadBtn'>ENTER</button>
+        <button id='SaveBtn'>SAVE</button>
       </div>
     );
   }
